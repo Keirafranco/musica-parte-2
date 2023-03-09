628843bb7755320dcfb9ed28aacd6a42753b4ebc
Keirafranco <125355714+Keirafranco@users.noreply.github.com>
2023-03-09 09:48:15 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
+})
+basic.forever(function () {
+	
+})
